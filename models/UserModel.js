b6a7f478fa685cbce089c2a6a91e3c12f659ace8
flowerStar323 <cloudmongo0001@gmail.com>
2021-09-04 23:24:06 +0900

    nfl-backend

---
+++ models/UserModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let User = new Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    countANDpay: [
+        {
+            count: {
+                type: Number
+            },
+            pay: {
+                type: Number
+            }
+        }
+    ],
+    status: {
+        type: Number,
+        default: 0
+    }
+
+
+},
+    { collection: "users" }
+);
+
+module.exports = mongoose.model('users', User);
+
+
